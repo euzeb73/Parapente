e30b88f6b3a2e84b623bb465f89d572d09c58d85
EuZeB <jimenez.lionel@gmail.com>
2022-11-27 22:54:27 +0100

    Test de carte de parapente

---
+++ carte.py
@@ -0,0 +1,81 @@
+
+from scipy import rand
+from sympy import C
+from settings import*
+import numpy as np
+from scipy.signal import convolve2d
+import matplotlib.pyplot as plt
+import random
+
+import mayavi.mlab as ma
+
+class Carte():
+    def __init__(self):
+        #taille 9x16
+        # 0,1,2,3 pour les hauteurs
+        self.mapdef = ['3100011122211100',
+                       '3100011000000023',
+                       '3000001111000022',
+                       '3310000112110033',
+                       '3332200122220002',
+                       '3332220011220001',
+                       '3332222000110002',
+                       '3333321111000003',
+                       '3333333222220000']
+        self.make_minimap()
+        self.make_map()
+
+    def make_minimap(self):
+        self.minimap = [[int(num) for num in line]
+                             for line in self.mapdef]
+
+    def make_map(self):
+        self.map = np.empty((HEIGHT//TILE_SIZE, WIDTH//TILE_SIZE))
+        height,width= self.map.shape
+        for i in range(height):
+            for j in range(width):
+                alti = self.minimap[i//10][j//10]  # à revoir avec des cstes
+                self.map[i, j] = HAUTEURS[alti]
+
+    def add_noise(self,noise_amp, noise_size : int):
+        '''
+        du bruit: d'amplitude noise_amp
+        de taille caractéristique noise_size
+        '''
+        height,width= self.map.shape
+        for i in range(0,height,noise_size):
+            for j in range(0,width,noise_size):
+                altshift=random.normalvariate(0,noise_amp)
+                tab_altshift=altshift*np.ones((noise_size,noise_size))
+                self.map[i:i+noise_size,j:j+noise_size]=self.map[i:i+noise_size,j:j+noise_size]+tab_altshift
+
+    def smoothen(self,N):
+        '''
+        un filtre moyenneur de taille NxN
+        '''
+        filtre=np.ones((N,N))
+        self.map = convolve2d(self.map,filtre,mode= 'same')/N**2
+
+    def plot3D(self):
+        height,width= self.map.shape
+        X = SCALE*np.arange(width)
+        Y = SCALE*np.arange(height)
+        X, Y = np.meshgrid(X, Y)
+        X = X.transpose()  # là y'a transposition d'après le manuel.
+        Y = Y.transpose()
+        fig = ma.figure()
+        # pour que ça colle transposition+renversement des 2 axes
+        tabalti = np.flip(self.map.transpose(), 1)
+        ma.surf(X, Y, tabalti, colormap='gist_earth')
+        ma.show()
+
+
+
+carte=Carte()
+carte.add_noise(250,5)
+carte.smoothen(10)
+carte.plot3D()
+plt.figure()
+plt.imshow(carte.map)
+plt.show()
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from settings import *
+
+
+
+class App:
+    def __init__(self):
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.time = 0
+        # self.delta_time = 0.01
+       
+        # game objects
+        self.terrain=Terrain()
+
+    def update(self):
+        self.scene.update()
+        self.main_group.update()
+        pg.display.set_caption(f'{self.clock.get_fps(): .1f}')
+        self.delta_time = self.clock.tick()
+
+    def draw(self):
+        self.screen.fill(BG_COLOR)
+        self.main_group.draw(self.screen)
+        pg.display.flip()
+
+    def check_events(self):
+        self.anim_trigger = False
+        for e in pg.event.get():
+            if e.type == pg.QUIT or (e.type == pg.KEYDOWN and e.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+            elif e.type == self.anim_event:
+                self.anim_trigger = True
+
+    def get_time(self):
+        self.time = pg.time.get_ticks() * 0.001
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.get_time()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
